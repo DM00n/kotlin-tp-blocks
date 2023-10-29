5ba132364cf77e90215b9f300db40d14d47f8d63
DM00n <teamvortex@yandex.ru>
2023-10-29 21:48:10 +0300

    recyclerView try (nw)

--- app/src/main/java/com/example/blocks/MainActivity.kt
+++ app/src/main/java/com/example/blocks/MainActivity.kt
@@ -10,44 +10,57 @@ import android.widget.GridLayout
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.setPadding
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.google.android.material.floatingactionbutton.FloatingActionButton
 
 class MainActivity : AppCompatActivity() {
 
     private var count: Int = 0
     private lateinit var fab: FloatingActionButton
-    private lateinit var gridLayout: GridLayout
+    //private lateinit var gridLayout: GridLayout
+    private lateinit var recyclerView: RecyclerView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         fab = findViewById(R.id.fab)
-        gridLayout = findViewById(R.id.gridLayout)
+        recyclerView = findViewById(R.id.recycleView)
+
+        val gridLayoutManager =  GridLayoutManager(this, if (this.resources.configuration.orientation == Configuration.ORIENTATION_LANDSCAPE) 4 else 3)
+        recyclerView.layoutManager = gridLayoutManager
+        //gridLayout = findViewById(R.id.gridLayout)
 
         savedInstanceState?.let {
-            gridLayout.columnCount = if (this.resources.configuration.orientation == Configuration.ORIENTATION_LANDSCAPE) 4 else 3
+
+            //gridLayout.columnCount = if (this.resources.configuration.orientation == Configuration.ORIENTATION_LANDSCAPE) 4 else 3
             for (i in 0 until it.getInt("count")){
                 val tv = TextView(this)
-                tv.layoutParams = ViewGroup.LayoutParams(TypedValue.COMPLEX_UNIT_DIP * 300, TypedValue.COMPLEX_UNIT_DIP * 300)
+                tv.layoutParams = RecyclerView.LayoutParams(TypedValue.COMPLEX_UNIT_DIP * 300, TypedValue.COMPLEX_UNIT_DIP * 300)
                 tv.setPadding(10)
                 tv.textAlignment=View.TEXT_ALIGNMENT_CENTER
                 tv.textSize=TypedValue.COMPLEX_UNIT_SP.toFloat() * 30
                 tv.setBackgroundColor(if (i % 2 ==0 ) {Color.RED} else {Color.BLUE})
                 tv.text=i.toString()
-                gridLayout.addView(tv)
+                recyclerView.addView(tv)
+                gridLayoutManager.addView(tv)
+                //gridLayout.addView(tv)
             }
             count=it.getInt("count")
         }
 
         fab.setOnClickListener{
             val tv = TextView(this)
-            tv.layoutParams = ViewGroup.LayoutParams(TypedValue.COMPLEX_UNIT_DIP * 300, TypedValue.COMPLEX_UNIT_DIP * 300)
+            tv.layoutParams = RecyclerView.LayoutParams(TypedValue.COMPLEX_UNIT_DIP * 300, TypedValue.COMPLEX_UNIT_DIP * 300)
+
             tv.setPadding(10)
             tv.textAlignment=View.TEXT_ALIGNMENT_CENTER
             tv.textSize=TypedValue.COMPLEX_UNIT_SP.toFloat() * 30
             tv.setBackgroundColor(if (count % 2 ==0 ) {Color.RED} else {Color.BLUE})
             tv.text=count.toString()
-            gridLayout.addView(tv)
+            //gridLayout.addView(tv)
+            recyclerView.addView(tv)
+            gridLayoutManager.addView(tv)
             count++
         }
     }
