660a1b4a27299f44d11ee66965cd7a4272743ab8
DM00n <teamvortex@yandex.ru>
2023-10-30 13:03:05 +0300

    recyclerView integration

--- app/src/main/java/com/example/blocks/MainActivity.kt
+++ app/src/main/java/com/example/blocks/MainActivity.kt
@@ -1,72 +1,46 @@
 package com.example.blocks
 
 import android.content.res.Configuration
-import android.graphics.Color
 import android.os.Bundle
-import android.util.TypedValue
-import android.view.View
-import android.view.ViewGroup
-import android.widget.GridLayout
-import android.widget.TextView
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.setPadding
 import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.RecyclerView
-import com.google.android.material.floatingactionbutton.FloatingActionButton
 
 class MainActivity : AppCompatActivity() {
 
-    private var count: Int = 0
-    private lateinit var fab: FloatingActionButton
-    //private lateinit var gridLayout: GridLayout
     private lateinit var recyclerView: RecyclerView
+    private lateinit var fab: Button
+    private lateinit var adapter: MainAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        fab = findViewById(R.id.fab)
+        adapter = MainAdapter()
         recyclerView = findViewById(R.id.recycleView)
-
-        val gridLayoutManager =  GridLayoutManager(this, if (this.resources.configuration.orientation == Configuration.ORIENTATION_LANDSCAPE) 4 else 3)
-        recyclerView.layoutManager = gridLayoutManager
-        //gridLayout = findViewById(R.id.gridLayout)
-
-        savedInstanceState?.let {
-
-            //gridLayout.columnCount = if (this.resources.configuration.orientation == Configuration.ORIENTATION_LANDSCAPE) 4 else 3
-            for (i in 0 until it.getInt("count")){
-                val tv = TextView(this)
-                tv.layoutParams = RecyclerView.LayoutParams(TypedValue.COMPLEX_UNIT_DIP * 300, TypedValue.COMPLEX_UNIT_DIP * 300)
-                tv.setPadding(10)
-                tv.textAlignment=View.TEXT_ALIGNMENT_CENTER
-                tv.textSize=TypedValue.COMPLEX_UNIT_SP.toFloat() * 30
-                tv.setBackgroundColor(if (i % 2 ==0 ) {Color.RED} else {Color.BLUE})
-                tv.text=i.toString()
-                recyclerView.addView(tv)
-                gridLayoutManager.addView(tv)
-                //gridLayout.addView(tv)
+        recyclerView.adapter = adapter
+        recyclerView.layoutManager = GridLayoutManager(
+            this,
+            if (this.resources.configuration.orientation == Configuration.ORIENTATION_PORTRAIT) {
+                3
+            } else {
+                4
             }
-            count=it.getInt("count")
+        )
+        savedInstanceState?.let {
+            for (i in 0 until it.getInt("count")) {
+                adapter.items.add(i)
+            }
         }
-
-        fab.setOnClickListener{
-            val tv = TextView(this)
-            tv.layoutParams = RecyclerView.LayoutParams(TypedValue.COMPLEX_UNIT_DIP * 300, TypedValue.COMPLEX_UNIT_DIP * 300)
-
-            tv.setPadding(10)
-            tv.textAlignment=View.TEXT_ALIGNMENT_CENTER
-            tv.textSize=TypedValue.COMPLEX_UNIT_SP.toFloat() * 30
-            tv.setBackgroundColor(if (count % 2 ==0 ) {Color.RED} else {Color.BLUE})
-            tv.text=count.toString()
-            //gridLayout.addView(tv)
-            recyclerView.addView(tv)
-            gridLayoutManager.addView(tv)
-            count++
+        fab = findViewById(R.id.fab)
+        fab.setOnClickListener {
+            adapter.items.add(adapter.items.size - 1)
+            adapter.notifyItemInserted(adapter.items.size - 1)
         }
     }
 
     override fun onSaveInstanceState(outState: Bundle) {
         super.onSaveInstanceState(outState)
-        outState.putInt("count", count)
+        outState.putInt("count", adapter.itemCount)
     }
 }
